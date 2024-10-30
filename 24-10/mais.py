8e0d2fe85b1e6ec43e0c7fa11002e7e2eb1b76cf
LuizM19 <luizmariozenimattei@gmail.com>
2024-10-30 17:57:56 -0400

    Add files via upload

---
+++ 24-10/mais.py
@@ -0,0 +1,5 @@
+if 5 > 2:
+    print("Cinco é maior que dois")    
+    print("Cinco é maior que dois")
+    """ Esse jeito com 3 aspas forma um texto que nao aparece no terminal"""
+    #Esse é o comentario com hashttag
